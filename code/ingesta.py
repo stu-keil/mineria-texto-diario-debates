98411286ab940d8e88b4a94303eb48087c30544b
stu-keil <stephanekeil@gmail.com>
2016-05-13 04:47:18 -0500

    ingesta lista - entrada a doc2vec

--- code/ingesta.py
+++ code/ingesta.py
@@ -19,53 +19,72 @@ import math
 import pandas as pd
 import sys
 import gensim
-
+#Cleaning the house
 os.getcwd()
 os.chdir("/home/stuka/itam2/textmining/mineria-texto-diario-debates/data/raw/")
 os.listdir(".")
 
-path_to_raw = "/home/stuka/itam2/textmining/mineria-texto-diario-debates/data/raw/"
+#Debe venir de fuera
+path_to_raw = "/home/stuka/itam2/textmining/mineria-texto-diario-debates/data/"
 
-file_names = [f for f in os.listdir(path_to_raw) if f.endswith('.txt')]
+file_names = [f for f in os.listdir(path_to_raw+'raw/') if f.endswith('.txt')]
 
 documentos =[io.open(f,'rt',encoding='ISO-8859-1') for f in file_names]
 
 
-raw = data[0].read()
+for file_name in file_names:
+    sourceEncoding = "iso-8859-1"
+    targetEncoding = "utf-8"
+    source = io.open(file_name,'rt',encoding='ISO-8859-1')
+    target = open(path_to_raw+'encoded/'+file_name, "w")
+    target.write(source.read().encode(targetEncoding))
+    
+documentos =[io.open(path_to_raw+'encoded/'+f,'rt') for f in file_names]
+
+#Pruebas
+raw = documentos[0].read()
 
 documentos[1]
 
 dir(data[0])
 data[0].name
-
-len(documentos[0].read(20000).split("Honorable asamblea:"))
-
+#Codigo para probar stopterm
 for documento in documentos:
-    documento_nombre = documento.name.split(".")[0]
-    raw = documento.read()
-    tematicas = raw.split("Honorable asamblea:")
-    for tematica in range(len(documentos)):
-        contenido = tematicas[tematica]
-        documento_nombre+'_'tematica
-        
-        
-        
-
-class LabeledLineSentence(object):
-    def __init__(self, filename):
-        self.filename = filename
-    def __iter__(self):
-        for uid, line in enumerate(open(self.filename)):
-            yield gensim.models.doc2vec.LabeledSentence(words=line.split(), labels=['TXT_%s' % uid])
-
-
-sentences = LabeledLineSentence(documentos[0])
-for line in sentences:
-    print(line)
-
-lines = data[0].readlines()
-listota [] 
-for line in lines:
-    print(len(line.split(".")))
+    print(len(documento.read().replace("Honorable Asamblea:","Honorable asamblea:").split("Honorable asamblea:")))
     
-line = lines[0]
+    
+#Este es el buen codigo
+for documento in documentos:
+    documento_nombre = documento.name.rsplit('/',1)[1].split('.')[0]
+    raw = documento.read()
+    for i,tematica in enumerate(raw.replace("Honorable Asamblea:","Honorable asamblea:").split("Honorable asamblea:")):
+        with io.open(path_to_raw+'tematicas/'+documento_nombre+'_'+str(i+1)+'.txt', mode="w") as newfile:
+            newfile.write(tematica.replace('\n',' '))
+ 
+       
+file_names_tematicas = [f for f in os.listdir(path_to_raw+'tematicas/') if f.endswith('.txt')]
+sentences = []
+i=0
+for doc in file_names_tematicas:
+    f = io.open(path_to_raw+'tematicas/'+doc,'rt')
+    sentences.append(gensim.models.doc2vec.LabeledSentence(f.read().split(),["SENT_"+str(i+1)]))
+    i=i+1
+    f.close()
+
+sentences[0]
+sentences[0:5]
+print(sentences[0:5]) 
+
+test = sentences[0:5]
+
+min_count = 2
+size = 50
+window = 4
+ 
+model = gensim.models.doc2vec.Doc2Vec(sentences,size = size, window = window, min_count = min_count)
+dir(model)
+model.vocab
+model.similarity('organismos','constar')
+model.most_similar('hombre')
+print model.docvecs.most_similar(["SENT_1"])
+model.docvecs["SENT_1"]
